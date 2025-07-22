a4125bcc217ecc8b2b7f61d6e820cb4f3baa4091
Sorin Marin <sorin.marin86@yahoo.com>
2025-07-22 11:02:00 +0100

    changes to the project st

--- tests/sorin/2.st.register.spec.ts
+++ tests/sorin/2.st.register.spec.ts
@@ -1,22 +1,10 @@
 import { expect, test } from "@playwright/test";
+import { register } from "module";
 
 test.only('register user', async ({page}) =>{
 
-// await page.goto('https://www.swimtime.org/');
 await page.goto('https://portal.swimtime.org/register');
 await page.getByRole('button', { name: 'Allow all' }).click();
-// await page.getByRole('link', {name: 'My Account'}).click();
-
-// // 1. Get the browser context
-// const context = page.context();
-//   // 2. Set up new tab listener BEFORE clicking
-// const newTabPromise = context.waitForEvent('page');
-// await page.getByRole('link', { name: 'My Account' }).click();
-// const newTab = await newTabPromise;
-// await newTab.waitForLoadState('domcontentloaded');
-// console.log('New tab URL:', await newTab.url());
-
-
 
 const name = 'John';
 const lastName = 'Doe';
@@ -29,13 +17,29 @@ const postcode = 'WR25AD';
 
 await page.locator('input[type="email"]').fill(email);
 await page.locator('input[type="password"]').fill(password);
-await page.getByRole('textbox', { name: 'First Name required' }).fill(name);
-await page.getByRole('textbox', { name: 'Last Name required' }).fill(lastName);
+await page.getByRole('textbox', {name: 'First Name required' }).fill(name);
+await page.getByRole('textbox', {name: 'Last Name required' }).fill(lastName);
 await page.getByRole('textbox', {name:'Primary Contact Number'}).fill(mobile);
 await page.getByRole('textbox', {name: 'Address'}).nth(0).fill(address);
 await page.getByRole('textbox', {name: 'City'}).fill(city);
 await page.getByRole('textbox', {name: 'Postcode'}).fill(postcode);
+
+const register = await page.getByRole('button', {name: 'Register'});
+await expect(register).toBeDisabled();
+await expect(register).toBeVisible();
+
 await page.getByLabel('How did you hear about us?').selectOption('Internet Search');
+
+await expect(register).toBeEnabled();
+await expect(register).toBeVisible();
+
+const phone = await page.getByText('Phone');
+await phone.click();
+await expect(phone).toBeChecked();
+
+const sms = await page.getByText('SMS');
+await sms.click();
+await expect(sms).toBeChecked();
 
 const header = await page.getByRole('heading', {name: 'Account Owner Details'});
 await expect(header).toBeVisible();
